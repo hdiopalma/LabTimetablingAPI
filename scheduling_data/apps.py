687276ec151577b8f866c0c2935b48d662e70906
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-05-09 10:10:35 +0700

    Import scheduling data signals when app is ready

--- scheduling_data/apps.py
+++ scheduling_data/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SchedulingDataConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "scheduling_data"
+    
+    def ready(self) -> None:
+        import scheduling_data.utils.signals
