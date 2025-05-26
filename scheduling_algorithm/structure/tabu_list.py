eef99c6f84275f871399e76fde1fb6dfcec23c8f
Hernando Dio Palma <hdiopalma@gmail.com>
2025-05-26 21:45:28 +0700

    Refactor TabuList class to improve clarity and consistency; streamline methods and update variable names for better understanding

--- scheduling_algorithm/structure/tabu_list.py
+++ scheduling_algorithm/structure/tabu_list.py
@@ -1,42 +1,28 @@
 from collections import deque
-from scheduling_algorithm.structure import Chromosome
+from typing import Tuple
 
 class TabuList:
-    def __init__(self, tabu_list_size: int = 10):
-        self.tabu_list_size = tabu_list_size
-        self.tabu_list = deque(maxlen=tabu_list_size)
+    def __init__(self, size: int = 50):
+        self.size = size
+        self.tabu_moves = deque(maxlen=size)
     
     def __str__(self):
-        return f"TabuList(tabu_list_size={self.tabu_list_size}, tabu_list={self.tabu_list})"
+        return f"TabuList(size={self.size}, tabu_moves={list(self.tabu_moves)})"
     
-    def __contains__(self, chromosome: Chromosome):
-        return chromosome in self.tabu_list
+    def __contains__(self, move: Tuple[int, int]):
+        return move in self.tabu_moves
     
-    def __len__(self):
-        return len(self.tabu_list)
+    def add(self, move: Tuple[int, int]):
+        self.tabu_moves.append(move)
     
-    def __getitem__(self, index):
-        return self.tabu_list[index]
+    def clear(self):
+        self.tabu_moves.clear()
     
-    def __setitem__(self, index, chromosome: Chromosome):
-        self.tabu_list[index] = chromosome
+    def is_tabu(self, move: Tuple[int, int]):
+        """Check if a move is in the tabu list."""
+        return move in self.tabu_moves
     
-    def __delitem__(self, index):
-        del self.tabu_list[index]
-    
-    def __iter__(self):
-        return iter(self.tabu_list)
-    
-    def __reversed__(self):
-        return reversed(self.tabu_list)
-    
-    def __add__(self, chromosome: Chromosome):
-        self.tabu_list.append(chromosome)
-    
-    # def is_tabu(self, chromosome: Chromosome) -> bool:
-    #     return chromosome in self.tabu_list
-    
-    def configure(self, tabu_list_size: int):
-        self.tabu_list_size = tabu_list_size
-        self.tabu_list = deque(maxlen=tabu_list_size)
+    def configure(self, size: int):
+        self.size = size
+        self.tabu_moves = deque(maxlen=size)
         return self
